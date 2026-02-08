ab8bf36d176206b6256624b73ab3bb905d569a9a
ChipironWeaver <239015086+ChipironWeaver@users.noreply.github.com>
2026-02-08 19:50:29 +0100

    [Minor] Heart Renderer
    Issue : Hearts are really small :(

--- Assets/Scripts/HealthController.cs
+++ Assets/Scripts/HealthController.cs
@@ -1,17 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections;
 using UnityEngine;
+using Image = UnityEngine.UI.Image;
 
 public class HealthController : MonoBehaviour
 {
+    [Header("Health Settings")]
     [SerializeField] private int _currentHealth;
     [SerializeField] private int _maxHealth;
+    [Header("Invincibility Frame Settings")]
     [SerializeField] private float _invicibilityTime;
     [SerializeField] private bool  _invicible;
+    [Header("Render Settings")]
+    [SerializeField] private Sprite _heartSprite;
+    [SerializeField] private GameObject _group;
+    
+    private List<GameObject> _heartGroup = new List<GameObject>();
 
 
     public void Start()
     {
         _maxHealth = _currentHealth;
+        RenderHealth();
     }
 
     public void TakeDamage(int damage)
@@ -26,6 +37,7 @@ public class HealthController : MonoBehaviour
             _invicible = true;
             Invoke(nameof(RemoveInvincibility), _invicibilityTime);
         }
+        RenderHealth();
     }
 
     public void Heal(int heal)
@@ -35,6 +47,7 @@ public class HealthController : MonoBehaviour
         {
             _currentHealth = _maxHealth;
         }
+        RenderHealth();
     }
 
     public void Death()
@@ -45,5 +58,28 @@ public class HealthController : MonoBehaviour
     private void RemoveInvincibility()
     {
         _invicible = false;
+    }
+
+    private void RenderHealth()
+    {
+        while (_heartGroup.Count < _maxHealth)
+        {
+            _heartGroup.Add(InstantiateHeart());
+        }
+
+        for (int i = 0; i < _heartGroup.Count; i++)
+        {
+            if (i > _currentHealth - 1) _heartGroup[i].gameObject.SetActive(false);
+            else _heartGroup[i].gameObject.SetActive(true);
+        }
+    }
+
+    private GameObject InstantiateHeart()
+    {
+        GameObject Heart = new GameObject();
+        Heart.transform.SetParent(_group.transform);
+        Heart.name = "Heart" + (_heartGroup.Count + 1) ;
+        Heart.AddComponent<Image>().sprite = _heartSprite;
+        return Heart;
     }
 }
