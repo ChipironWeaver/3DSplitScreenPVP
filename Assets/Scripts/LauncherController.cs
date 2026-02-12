fde9af212168fb58e0e25539f7fde3c0e392d646
ChipironWeaver <lowen.tisserand@gmail.com>
2026-02-12 10:42:02 +0100

    [Major] Bullet Ui, reload WIP

--- Assets/Scripts/LauncherController.cs
+++ Assets/Scripts/LauncherController.cs
@@ -1,5 +1,8 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class LauncherController : MonoBehaviour
 {
@@ -7,30 +10,50 @@ public class LauncherController : MonoBehaviour
     [SerializeField] private GameObject _ammoPrefab;
     [SerializeField] private Transform _spawnPoint;
     [SerializeField] private float _firingCooldown;
+    [SerializeField] private int _maxAmmo;
     [Header("UX Settings")]
     [SerializeField] private AudioClip _firingSound;
     [SerializeField] private AudioClip _reloadSound;
     [SerializeField] private GameObject _shootParticle;
+    [Header("UI Settings")]
+    [SerializeField] private Sprite _emptySprite;
+    [SerializeField] private Sprite _fullSprite;
+    [SerializeField] private GameObject _ammoUIGroup;
     
     private bool _canFire = true;
+    private List<Image> _ammoUIImages = new List<Image>();
+    private int _currentAmmo;
+
+
+    private void Start()
+    {
+        _currentAmmo = _maxAmmo;
+        RenderAmmo();
+    }
 
     private void OnFire(InputValue value)
     {
         if (_canFire)
         {
-            GameObject instantiate = Instantiate(_ammoPrefab, _spawnPoint.position, Quaternion.identity);
-            Rigidbody rb = instantiate.GetComponent<Rigidbody>();
-            rb.AddForce(_spawnPoint.forward * _ammoPower);
-            _canFire = false;
-            Invoke(nameof(ResetFire), _firingCooldown);
-            if (_firingSound != null)
+            if (_currentAmmo > 0)
             {
-                AudioSource.PlayClipAtPoint(_firingSound, Vector3.zero);
-            }
+                GameObject instantiate = Instantiate(_ammoPrefab, _spawnPoint.position, Quaternion.identity);
+                Rigidbody rb = instantiate.GetComponent<Rigidbody>();
+                rb.AddForce(_spawnPoint.forward * _ammoPower);
+                _canFire = false;
+                Invoke(nameof(ResetFire), _firingCooldown);
+                if (_firingSound != null)
+                {
+                    AudioSource.PlayClipAtPoint(_firingSound, Vector3.zero);
+                }
 
-            if (_shootParticle != null)
-            {
-                Instantiate(_shootParticle, _spawnPoint.position, Quaternion.identity);
+                if (_shootParticle != null)
+                {
+                    Instantiate(_shootParticle, _spawnPoint.position, Quaternion.identity);
+                }
+
+                _currentAmmo--;
+                RenderAmmo();
             }
         }
         
@@ -40,4 +63,27 @@ public class LauncherController : MonoBehaviour
     {
         _canFire = true;
     }
+
+    private void RenderAmmo()
+    {
+        while (_ammoUIImages.Count < _maxAmmo)
+        {
+            _ammoUIImages.Add(InstantiateAmmoUI());
+        }
+        for (int i = 0; i < _ammoUIImages.Count; i++)
+        {
+            if (i > _currentAmmo - 1) _ammoUIImages[i].sprite = _emptySprite;
+            else _ammoUIImages[i].sprite = _fullSprite;
+        }
+    }
+
+    private Image InstantiateAmmoUI()
+    {
+        GameObject AmmoInstance = new GameObject();
+        AmmoInstance.transform.SetParent(_ammoUIGroup.transform);
+        AmmoInstance.name = "Ammo" + (_ammoUIImages.Count + 1) ;
+        AmmoInstance.AddComponent<RectTransform>().localScale = Vector3.one;
+        return AmmoInstance.AddComponent<Image>();
+    }
+    
 }
